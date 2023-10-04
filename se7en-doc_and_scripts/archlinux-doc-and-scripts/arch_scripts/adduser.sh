43ddcfa60f07ae9d0bb818f96093521fac16ab13
lcdse7en <2353442022@qq.com>
2023-10-04 21:46:30 +0800

    add

---
+++ se7en-doc_and_scripts/archlinux-doc-and-scripts/arch_scripts/adduser.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/bash
+
+#********************************************
+# Author      : lcdse7en
+# E-mail      : 2353442022@qq.com
+# Create_Time : 2023-10-04 21:32
+# Description :
+#********************************************
+
+# source /etc/profile.d/import_shell_libs.sh
+
+RED=$(printf '\033[31m')
+GREEN=$(printf '\033[32m')
+YELLOW=$(printf '\033[33m')
+BLUE=$(printf '\033[34m')
+SKYBLUE=$(printf '\033[36m')
+BOLD=$(printf '\033[1m')
+RESET=$(printf '\033[m')
+
+AddUser() {
+    useradd -m -G wheel se7en
+    passwd se7en <<-EOF
+	921216
+	921216
+	EOF
+}
+
+Wheel() {
+    sudo sed -i 's/^# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
+    sudo sed -i 's/^# %wheel ALL=(ALL:ALL) NOPASSWD: ALL%/%wheel ALL=(ALL:ALL) NOPASSWD: ALL/' /etc/sudoers
+}
+
+main() {
+    # AddUser
+    Wheel
+}
+
+main
