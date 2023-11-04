cf9ccc1f7f02fc2b16e1957e1e214afcd0eca95b
lcdse7en <2353442022@qq.com>
2023-11-04 15:21:21 +0800

    add

---
+++ se7en-doc_and_scripts/archlinux-doc-and-scripts/arch_scripts/clone.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/bash
+
+#********************************************
+# Author      : lcdse7en                    *
+# E-mail      : 2353442022@qq.com           *
+# Create_Time : 2023-11-04 15:00            *
+# Description :                             *
+#********************************************
+
+# source /etc/profile.d/import_shell_libs.sh
+
+RED=$(printf '\033[31m')
+GREEN=$(printf '\033[32m')
+YELLOW=$(printf '\033[33m')
+BLUE=$(printf '\033[34m')
+SKYBLUE=$(printf '\033[36m')
+BOLD=$(printf '\033[1m')
+RESET=$(printf '\033[m')
+
+Clone() {
+    local target_dir="$HOME/github_upload"
+
+    if [[ -d "$target_dir" ]]; then
+        cd "$target_dir" || exit
+        git clone git@github.com:lcdse7en/lyrics.git
+        git clone git@github.com:lcdse7en/pandas.git
+        git clone git@github.com:lcdse7en/os.git
+    else
+        printf "${SKYBLUE}%s"
+        printf "*%.0s" {1..60}
+        printf "${RESET}%s\n"
+        printf "Creating dir: ${target_dir} ...%s"
+        printf "${SKYBLUE}%s"
+        printf "*%.0s" {1..60}
+        printf "${RESET}%s\n"
+
+        mkdir "$target_dir"
+
+        cd "$target_dir" || exit
+        git clone git@github.com:lcdse7en/lyrics.git
+        git clone git@github.com:lcdse7en/pandas.git
+        git clone git@github.com:lcdse7en/os.git
+    fi
+}
+
+main() {
+    Clone
+}
+
+main
