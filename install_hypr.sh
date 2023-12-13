8bbb966db615c267a8e85b538277895ca0115da0
lcdse7en <2353442022@qq.com>
2023-12-13 20:06:26 +0800

    add

--- install_hypr.sh
+++ install_hypr.sh
@@ -473,6 +473,38 @@ Python() {
     done
 }
 
+Fcitx5() {
+    local fcitx_packages=(
+        fcitx5-im
+        fcitx5-chinese-addons
+        fcitx5-material-color
+        fcitx5-nord
+        fcitx5-lua
+        fcitx5-pinyin-zhwiki
+        fcitx5-pinyin-moegirl
+        fcitx5-hangul
+        kcm-fcitx5
+        fcitx5-anthy
+        fcitx5-mozc
+    )
+    sudo pacman -S --noconfirm "${fcitx_packages[@]}"
+
+    sudo tee /etc/environment <<-EOF
+		GTK_IM_MODULE=fcitx
+		QT_IM_MODULE=fcitx
+		XMODIFIERS=@im=fcitx
+		INPUT_METHOD=fcitx
+		SDL_IM_MODULE=fcitx
+		GLFW_IM_MODULE=ibus
+	EOF
+    sudo tee ~/.pam_enviroment <<-EOF
+		GTK_IM_MODULE DEFAULT=fcitx5
+		QT_IM_MODULE  DEFAULT=fcitx5
+		XMODIFIERS    DEFAULT=@im=fcitx5
+	EOF
+
+}
+
 main() {
     # Yay_packages
     # Paru_packages
@@ -483,6 +515,7 @@ main() {
     Stow_Dir
     # Font
     # Python
+    Fcitx5
 }
 
 main
