400f6271eebe6e40a59be3f2d9c49201b79d23ae
lcdse7en <2353442022@qq.com>
2023-12-14 13:49:50 +0800

    add

--- install_hypr.sh
+++ install_hypr.sh
@@ -514,6 +514,126 @@ Fcitx5() {
 				QT_IM_MODULE  DEFAULT=fcitx5
 				XMODIFIERS    DEFAULT=@im=fcitx5
 		EOF
+
+    local fcitx5confpath="$HOME/.config/fcitx5"
+    if [[ ! -d "$fcitx5confpath" ]]; then
+        cd "$HOME/dotfiles"
+        stow fcitx5
+    else
+        printf "${RED}Directory $fcitx5confpath already exists.${RESET}\n"
+
+    fi
+}
+
+Zsh() {
+    printf "${YELLOW}%s"
+    read -r -p "Do you want to Install oh-my-zsh on you ArchLinux? [y/N] " answer
+    printf "${RESET}%s"
+
+    if [[ "$answer" != y ]] && [[ "$answer" != Y ]]; then
+        printf "${RED}%s"
+        printf "Skipping install oh-my-zsh on your machine.\n"
+        printf "${RESET}%s"
+    else
+        sh "$HOME/dotfiles/install-ohmyzsh.sh"
+        sleep 1
+        sudo rm -rf "$HOME"/.zshrc >/dev/null
+        cd "$HOME/dotfiles"
+        stow zsh
+    fi
+
+    printf "${YELLOW}%s\n"
+    read -r -p "Configure oh-my-zsh, Download ohmyzsh plugins? [y/N] " answer
+    printf "${RESET}%s"
+
+    if [[ "$answer" != y ]] && [[ "$answer" != Y ]]; then
+        printf "${RED}%s"
+        printf "Skipping download ohmyzsh plugins.\n"
+        printf "${RESET}%s\n"
+    else
+        local autosuggestions_dir="$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions"
+        local highlighting_dir="$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting"
+        local autojump_dir="$HOME/.oh-my-zsh/custom/plugins/autojump"
+        local vimode_dir="$HOME/.oh-my-zsh/custom/plugins/zsh-vi-mode"
+        local you_should_use="$HOME/.oh-my-zsh/custom/plugins/you-should-use"
+        local zsh_history_substring_search="$HOME/.oh-my-zsh/custom/plugins/zsh-history-substring-search"
+
+        #  NOTE: zsh-autosuggestions
+        if [[ -d "$autosuggestions_dir" ]]; then
+            printf "${RED}%s"
+            printf "Directory: %s already exists, Skipping cloning.\n" "$autosuggestions_dir"
+            printf "${RESET}%s\n"
+        else
+            printf "$YELLOW%s$RESET\n" "zsh-autosuggestions ..."
+            printf "$SKYBLUE*%.0s" {1..50}
+            printf "\n"
+            git clone https://github.com/zsh-users/zsh-autosuggestions.git \
+                "$HOME"/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+        fi
+        #  NOTE: zsh-syntax-highlighting
+        if [[ -d "$highlighting_dir" ]]; then
+            printf "${RED}%s"
+            printf "Directory: %s already exists, Skipping cloning.\n" "$highlighting_dir"
+            printf "${RESET}%s\n"
+        else
+            printf "$YELLOW%s$RESET\n" "Downloading zsh-syntax-highlighting ..."
+            printf "$SKYBLUE*%.0s" {1..50}
+            printf "\n"
+            git clone https://github.com/zsh-users/zsh-syntax-highlighting.git \
+                "$HOME"/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+        fi
+        #  NOTE: autojump
+        if [[ -d "$autojump_dir" ]]; then
+            printf "${RED}%s"
+            printf "Directory: %s already exists, Skipping cloning.\n" "$autojump_dir"
+            printf "${RESET}%s\n"
+        else
+            printf "$YELLOW%s$RESET\n" "Downloading autojump ..."
+            printf "$SKYBLUE*%.0s" {1..50}
+            printf "\n"
+            git clone https://github.com/wting/autojump.git \
+                "$HOME"/.oh-my-zsh/custom/plugins/autojump
+        fi
+        #  NOTE: zsh-vi-mode
+        if [[ -d "$vimode_dir" ]]; then
+            printf "${RED}%s"
+            printf "Directory: %s already exists, Skipping cloning.\n" "$vimode_dir"
+            printf "${RESET}%s\n"
+        else
+            printf "$YELLOW%s$RESET\n" "Downloading zsh-vi-mode ..."
+            printf "$SKYBLUE*%.0s" {1..50}
+            printf "\n"
+            git clone https://github.com/jeffreytse/zsh-vi-mode.git \
+                "$HOME"/.oh-my-zsh/custom/plugins/zsh-vi-mode
+            # ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-vi-mode
+        fi
+        #  NOTE: you-should-use
+        if [[ -d "$you_should_use" ]]; then
+            printf "${RED}%s"
+            printf "Directory: %s already exists, Skipping cloning.\n" "$you_should_use"
+            printf "${RESET}%s\n"
+        else
+            printf "$YELLOW%s$RESET\n" "Downloading you-should-use ..."
+            printf "$SKYBLUE*%.0s" {1..50}
+            printf "\n"
+            git clone https://github.com/MichaelAquilina/zsh-you-should-use.git \
+                "$HOME"/.oh-my-zsh/custom/plugins/you-should-use
+            # ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-vi-mode
+        fi
+        #  NOTE: zsh-history-substring-search
+        if [[ -d "$zsh_history_substring_search" ]]; then
+            printf "${RED}%s"
+            printf "Directory: %s already exists, Skipping cloning.\n" "$zsh_history_substring_search"
+            printf "${RESET}%s\n"
+        else
+            printf "$YELLOW%s$RESET\n" "Downloading zsh-history-substring-search ..."
+            printf "$SKYBLUE*%.0s" {1..50}
+            printf "\n"
+            git clone https://github.com/zsh-users/zsh-history-substring-search.git \
+                "$HOME"/.oh-my-zsh/custom/plugins/zsh-history-substring-search
+            # ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-vi-mode
+        fi
+    fi
 }
 
 main() {
@@ -527,6 +647,7 @@ main() {
     # Font
     Python
     # Fcitx5
+    Zsh
 }
 
 main
