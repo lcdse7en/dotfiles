376fc0689696f5f3257faf6a3c00091948944a29
lcdse7en <2353442022@qq.com>
2023-12-14 18:42:55 +0800

    add

---
+++ hypr/.config/hypr/autostart.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+#********************************************
+# Author      : lcdse7en                    *
+# E-mail      : 2353442022@qq.com           *
+# Create_Time : 2023-12-14 18:41:47         *
+# Description :                             *
+#********************************************
+
+sudo systemctl enable redis
+sudo systemctl start redis
